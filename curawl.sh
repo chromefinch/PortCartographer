088a5786522700530e3ccb36dc15360618d1c1f5
chromefinch <38330372+chromefinch@users.noreply.github.com>
2025-03-11 17:11:37 -0400

    Update curawl.sh

--- curawl.sh
+++ curawl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 print_purple (){
-	echo -e "\033[0;35m$1\033[0m"
+    echo -e "\033[0;35m$1\033[0m"
 }
+
 print_purple "Hi! I'm painfully slow garbage!"
 if [[ -z "$1" ]]; then
   echo "Usage: $0 <URL>"
@@ -17,34 +18,44 @@ fi
 echo "Crawling domain: $domain"
 tmp_dir=$(mktemp -d)
 seen_file="$tmp_dir/seen_$domain.txt"
-visited_file="curawled_$domain.txt"
+visited_file="crawled_$domain.txt"
 echo "$1" > "$visited_file"
 
-curl -s "$1" | grep -Eo '=\"\/[.a-z,A-Z\/-]{1,}\"' | sed "s|=\"|$1|g" | sed "s|\"||g" | sort -u > "$seen_file"
+# Initial scrape of links
+curl -s "$1" | grep -Eo '=\"\/[.a-z,A-Z\/-]{1,}\"|https?://[^"]+' | sed "s|=\"|$1|g" | sed "s|\"||g" | sort -u > "$seen_file"
 stop=$(wc -l "$seen_file" | awk '{print $1}')
 start=0
 print_purple "Starting with $stop links found."
+
 while [[ $start -ne $stop ]]; do
   start=$stop
   for i in $(cat "$seen_file"); do
     if grep -qFx "$i" "$visited_file"; then
       echo "Duplicate: $i" >/dev/null 2>&1; 
     else
-      echo "Crawling: $i"
-        curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -silent $i | grep -Eo '=\"\/[.a-z,A-Z\/-]{1,}\"' | sed "s|=\"|$1|g" | sed "s|\"||g"   >> "$seen_file"
-      echo "$i" >> "$visited_file"
+      # Check if the link matches the domain
+      if [[ "$i" == *"$domain"* ]]; then
+        echo "Crawling: $i"
+        # Crawl only domain-matching links
+        curl -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s "$i" | grep -Eo '=\"\/[#.a-z,A-Z\/-]{1,}\"|https?://[^"]+|href=\"/[^\"]*\\' | sed "s|href=\"|$1|g" | sed "s|=\"|$1|g" | sed "s|\"||g" | grep -E 'https?://+' >> "$seen_file"
+        echo "$i" | grep -E 'https?://+' >> "$visited_file"
+      else
+        # Add non-domain links to seen_file only
+        echo "Adding non-domain link to seen file: $i"
+        echo "$i" | grep -E 'https?://+' >> "$visited_file"
+      fi
     fi
   done
 
   sort -u "$seen_file" -o "$seen_file"
   sort -u "$visited_file" -o "$visited_file"
   stop=$(wc -l "$seen_file" | awk '{print $1}')
-  print_purple  "Progress: $stop links visited."
+  print_purple "Progress: $stop links visited."
 done
 
 total=$(wc -l "$visited_file" | awk '{print $1}')
 cat "$visited_file"
-print_purple  "Finished! Total links visited: $total"
+print_purple "Finished! Total links visited: $total"
 
 # Cleanup
 rm -r "$tmp_dir"
